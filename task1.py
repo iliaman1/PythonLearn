de64d3c86b5915f5aa1e47ef854cfcf898379807
iliaman1 <iliaman1993@gmail.com>
2016-06-28 18:23:03 +0300

    Added task2.py

--- task1.py
+++ task1.py
@@ -2,5 +2,7 @@ name = input("What'is you name: ")
 
 if name is "Ilya":
     print ("Hello Ilya bitch")
+elif name is "Andrey":
+    print ("Hello clever boy")
 else:
     print ("Hello %s and fuck you" % name)
